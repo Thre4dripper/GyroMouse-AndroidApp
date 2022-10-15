7e3008ba2c4970191a340f2b4f13bb7fd1ba6846
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-10-15 16:16:08 +0530

    got device orientation from sensors

--- app/src/main/java/com/example/gyromouse/MainActivity.kt
+++ app/src/main/java/com/example/gyromouse/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.gyromouse
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -10,11 +14,22 @@ import java.io.BufferedWriter
 import java.io.OutputStreamWriter
 import java.net.Socket
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private val TAG = "MainActivity"
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     private lateinit var socketThread: Thread
+
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+    private var magnetometer: Sensor? = null
+    private var lastAccelerometer = FloatArray(3)
+    private var lastMagnetometer = FloatArray(3)
+    private var lastAccelerometerSet = false
+    private var lastMagnetometerSet = false
+    private var rotationMatrix = FloatArray(9)
+    private var orientation = FloatArray(3)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +38,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         connectToServer("192.168.0.102")
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
+        setGyroListener()
     }
 
     private fun connectToServer(ip: String) {
@@ -49,10 +69,52 @@ class MainActivity : AppCompatActivity() {
         socketThread.start()
     }
 
+    private fun setGyroListener() {
+
+    }
+
     override fun onStop() {
         super.onStop()
         viewModel.socket!!.close()
         socketThread.join()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lastAccelerometerSet = false
+        lastMagnetometerSet = false
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        if (p0!!.sensor == accelerometer) {
+            System.arraycopy(p0.values, 0, lastAccelerometer, 0, p0.values.size)
+            lastAccelerometerSet = true
+        } else if (p0.sensor == magnetometer) {
+            System.arraycopy(p0.values, 0, lastMagnetometer, 0, p0.values.size)
+            lastMagnetometerSet = true
+        }
+
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rotationMatrix, null, lastAccelerometer, lastMagnetometer)
+            SensorManager.getOrientation(rotationMatrix, orientation)
+
+            val azimuth = Math.toDegrees(orientation[0].toDouble()).toInt()
+            val pitch = Math.toDegrees(orientation[1].toDouble()).toInt()
+            val roll = Math.toDegrees(orientation[2].toDouble()).toInt()
+
+            binding.textView.text = "Azimuth: $azimuth Pitch: $pitch Roll: $roll"
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        return
     }
 }
 
